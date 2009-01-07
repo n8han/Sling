112ccbe4a8cd46a74885288a491e8d6798425da9
Nathan Hamblen <nathan@technically.us>
2009-01-07 01:38:35 -0500

    server ajp support and script

--- project/build/src/FridayBuild.scala
+++ project/build/src/FridayBuild.scala
@@ -14,7 +14,7 @@ class FridayBuild(info: ProjectInfo) extends DefaultProject(info)
   val snapshots = "Databinder Snapshots" at "http://databinder.net/snapshot/"
   val scala_tools = "Scala Tools Releases" at "http://scala-tools.org/repo-releases"
 
-  val jetty = "org.mortbay.jetty" % "jetty" % "6.1.14"
+  val jetty = "org.mortbay.jetty" % "jetty-ajp" % "6.1.14"
   val dispatch = "net.databinder" % "databinder-dispatch" % "1.2.2-SNAPSHOT"
   val rhino = "rhino" % "js" % "1.7R1"
   val scalaz = "com.workingmouse" % "scalaz" % "3.0"
@@ -53,6 +53,16 @@ class FridayBuild(info: ProjectInfo) extends DefaultProject(info)
     }
     None
   }
-  
   override def compileAction = super.compileAction dependsOn(showdown)
+  
+  lazy val script = task {
+    FileUtilities.writeStream((info.projectPath / "server.sh").asFile, log) { out =>
+      out write (
+"""#! /bin/sh
+
+$JAVA_HOME/bin/java -server -cp """ + runClasspath.get.mkString(":") + """ $JAVA_OPTIONS net.friday.Server
+""").getBytes
+      None
+    }
+  }
 }
--- src/main/scala/net/friday/Server.scala
+++ src/main/scala/net/friday/Server.scala
@@ -3,28 +3,30 @@ package net.friday
 import org.mortbay.jetty.Connector
 import org.mortbay.jetty.{Server => JettyServer}
 import org.mortbay.jetty.webapp.WebAppContext
+import org.mortbay.jetty.ajp.Ajp13SocketConnector
 
 object Server extends Application {
-  val server = new JettyServer(8080)
-  val context = new WebAppContext()
-  context.setServer(server)
-  context.setContextPath("/")
-  context.setWar("src/main/webapp")
+  def context(server: JettyServer) = new WebAppContext(server, "src/main/webapp", "/")
 
-  server.addHandler(context)
-
-  try {
-    println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP")
-    server.start()
-    while (System.in.available() == 0) {
-      Thread.sleep(5000)
-    }
-    server.stop()
-    server.join()
-  } catch {
-    case exc : Exception => {
-      exc.printStackTrace()
-      System.exit(100)
-    }
+  System.getProperty("jetty.ajp.port") match {
+    case null =>
+      val server = new JettyServer(8080)
+      server.addHandler(context(server))
+      println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP")
+      server.start()
+      while (System.in.available() == 0) {
+        Thread.sleep(1000)
+      }
+      server.stop()
+      server.join()
+    case p =>
+      val conn = new Ajp13SocketConnector()
+      conn.setPort(p.toInt)
+      val server = new JettyServer()
+      server.addConnector(conn)
+      server.addHandler(context(server))
+      server.setStopAtShutdown(true)
+      server.start()
+      server.join()
   }
 }
