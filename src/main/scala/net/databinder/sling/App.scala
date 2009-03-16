714f84751a5cd32886926e080add67d4dc2ca2af
Nathan Hamblen <nathan@technically.us>
2009-03-16 04:38:56 -0400

    craptacular

--- src/main/scala/net/databinder/sling/App.scala
+++ src/main/scala/net/databinder/sling/App.scala
@@ -7,7 +7,7 @@ import slinky.http.servlet.StreamStreamServletApplication.resourceOr
 import slinky.http.{ContentType, Date, CacheControl}
 import slinky.http.StreamStreamApplication._
 import slinky.http.request.Request.Stream.{MethodPath, Path}
-import slinky.http.request.{Request, GET, IfNoneMatch, RequestHeader}
+import slinky.http.request.{Request, GET, RequestHeader}
 import slinky.http.response.{Response, OK, NotFound, ETag, NotModified}
 import slinky.http.response.xhtml.Doctype.strict
 import scalaz.CharSet.UTF8
@@ -15,6 +15,7 @@ import scalaz.CharSet.UTF8
 import dispatch._
 import dispatch.couch._
 import dispatch.json._
+import dispatch.twitter.Search
 
 import org.apache.http.HttpResponse
 import org.apache.http.util.EntityUtils
@@ -62,44 +63,75 @@ object App {
       case _ => None
     }
   }
+  object ComboTag {
+    def unapplySeq(tag: String) = tag.replace("-gzip","").replace("\"","").split('|') match { // https://issues.apache.org/bugzilla/show_bug.cgi?id=39727
+      case ary if ary.length == 3 || ary.length == 1 => Some(ary)
+      case _ => None
+    }
+    def apply(couch_et: String) = couch_et
+    def apply(couch_et: String, tweed: String, latest_id: Number) = 
+      '"' + (couch_et.replaceAll("\"", "") :: tweed :: latest_id :: Nil).mkString("|") + '"'
+  }
   
   val showdown = new js.showdown()
   
   def couch = Couch(Configgy.config.getString("couch.host","127.0.0.1"))
   
-  def cache_heds(ri: HttpResponse, ro: Response[Stream]) = 
-    ro(Date, ri.getFirstHeader(Date).getValue)(CacheControl, "max-age=600")(ETag, ri.getFirstHeader(ETag).getValue)
+  def cache_heds(ri: HttpResponse, ro: Response[Stream], combo_tag: String) = 
+    ro(Date, ri.getFirstHeader(Date).getValue)(CacheControl, "max-age=600")(ETag, combo_tag)
   
   def app(implicit request: Request[Stream]) =
     request match {
       case Path(DbId(db, id)) =>
-        val couched = db( (couch /: request.headers) {
-          case (c, (k, v)) if k.asString == IfNoneMatch.asString =>
-            c << (k.asString, v.mkString.replace("-gzip","")) // gross: https://issues.apache.org/bugzilla/show_bug.cgi?id=39727
-          case (c, _) => c
-        })
-        
+        val IfNoneMatch = "If-None-Match"
+        val (hedded_couch, tweed_js) =
+          request.headers.find {
+            case (k, v) => k.asString == IfNoneMatch
+          } map { _._2.mkString } map {
+            case ComboTag(couch_et) =>
+              (couch << (IfNoneMatch, "\"" + couch_et + "\""), None)
+            case ComboTag(couch_et, tweed, latest) =>
+              val res = (new Search)(tweed)
+              res.firstOption.filter { case Search.id(id) => println(id); println(latest); id.toString == latest } map { js =>
+                (couch << (IfNoneMatch,  "\"" + couch_et+ "\""), Some(res))
+              } getOrElse { (couch, Some(res)) }
+            case _ => (couch, None)
+          } getOrElse (couch, None)
+
+        val couched = db(hedded_couch)
         couched(id) {
           case (OK.toInt, ri, Some(entity)) =>
+            val couch_et = ri.getFirstHeader(ETag).getValue
             id match {
               case ("style.css") =>
-                Some(cache_heds(ri, OK(ContentType, "text/css; charset=UTF-8")) << 
+                Some(cache_heds(ri, OK(ContentType, "text/css; charset=UTF-8"), ComboTag(couch_et)) << 
                   PageDoc.body(Js(entity.getContent())).toList
                 )
               case (id) if request !? "edit" =>
-                Some(cache_heds(ri, OK(ContentType, content_type)) << strict << 
+                Some(cache_heds(ri, OK(ContentType, content_type), ComboTag(couch_et)) << strict << 
                   Page(EditDocument(TOC(couched, id, "?edit"), 
                     EntityUtils.toString(entity, UTF8)
                   )).html
                 )
               case (id) =>
-                Some(cache_heds(ri, OK(ContentType, content_type)) << strict << 
-                  Page(ShowDocument(TOC(couched, id, ""), 
-                    Js(entity.getContent())
-                  )).html
+                val js = Js(entity.getContent())
+                val PageDoc.body(md) = js
+                val (combo_tag, tweedy) = js match {
+                  case PageDoc.tweed(t) => 
+                    val ljs = tweed_js.getOrElse { (new Search)(t) }
+                    val ct: String = ljs.firstOption.map {
+                      case Search.id(id) => ComboTag(couch_et, t, id)
+                    } getOrElse (ComboTag(couch_et))
+                    (ct, Some(t, ljs))
+                  case _ => (ComboTag(couch_et), None)
+                }
+                Some(cache_heds(ri, OK(ContentType, content_type), combo_tag) << strict << 
+                  Page(ShowDocument(TOC(couched, id, ""), md, tweedy)).html
                 )
             }
-          case (NotModified.toInt, ri, _) => Some(cache_heds(ri, NotModified))
+          case (NotModified.toInt, ri, _) => Some(cache_heds(ri, NotModified, 
+            // wrong etag if tweed present
+            ComboTag(ri.getFirstHeader(ETag).getValue)))
           case (NotFound.toInt, _, _) => None 
         }
 
@@ -147,49 +179,44 @@ object App {
     lazy val title = toc.db.name.capitalize + " → " + toc.curr_id
   }
   
-  case class ShowDocument(toc: TOC, js: JsValue) extends Document {
+  case class ShowDocument(toc: TOC, md: String, tweedy: Option[(String, List[JsValue])]) extends Document {
     def body =
       <div id="content">
         <div class="container">
           <h2>{ title }</h2>
           { toc.html }
+          { Unparsed(showdown.makeHtml(md).toString) } 
           {
-            val PageDoc.body(md) = js
-            Unparsed(showdown.makeHtml(md).toString) 
-          } {
-            js match {
-              case PageDoc.tweed(tweed) => 
-                <div>
-                  <h3>{ tweed } tweed</h3>
-                  <ul class="tweed"> {
-                    import dispatch.twitter.Search
-                    (new Search)(tweed) map { js =>
-                      val Search.text(text) = js
-                      val Search.from_user(from) = js
-                      val Search.created_at(time) = js
-                      val Search.id(id) = js
-                      val from_pg = "http://twitter.com/" + from
-                      <li>
-                        <a href={ from_pg }>{ from }</a>:
-                        { Unparsed(text) }
-                        <div>
-                          <em> { time.replace(" +0000", "") } </em>
-                          <a href={ "http://twitter.com/home?status=@" + from + 
-                            "%20&in_reply_to_status_id=" + id + "&in_reply_to=" + from
-                            }>Reply</a>
-                          <a href={ from_pg + "/statuses/" + id }>View Tweet</a>
-                        </div>
-                      </li>
-                    }
-                  } </ul>
-                  <p>
-                    <a href={ "http://search.twitter.com/search?q=" + tweed }>
-                      See all Twitter Search results for { tweed }
-                    </a>
-                  </p>
-                </div>
-              case _ => Nil
-            }
+            tweedy map { case (tweed, js) =>
+              <div>
+                <h3>{ tweed } tweed</h3>
+                <ul class="tweed"> {
+                  js map { js =>
+                    val Search.text(text) = js
+                    val Search.from_user(from) = js
+                    val Search.created_at(time) = js
+                    val Search.id(id) = js
+                    val from_pg = "http://twitter.com/" + from
+                    <li>
+                      <a href={ from_pg }>{ from }</a>:
+                      { Unparsed(text) }
+                      <div>
+                        <em> { time.replace(" +0000", "") } </em>
+                        <a href={ "http://twitter.com/home?status=@" + from + 
+                          "%20&in_reply_to_status_id=" + id + "&in_reply_to=" + from
+                          }>Reply</a>
+                        <a href={ from_pg + "/statuses/" + id }>View Tweet</a>
+                      </div>
+                    </li>
+                  }
+                } </ul>
+                <p>
+                  <a href={ "http://search.twitter.com/search?q=" + tweed }>
+                    See all Twitter Search results for { tweed }
+                  </a>
+                </p>
+              </div>
+            } toList
           }
         </div>
       </div>
