acf045ab603ac4d8cb68112a3eb1cd96a02bcd8d
Nathan Hamblen <nathan@technically.us>
2009-03-17 21:29:20 -0400

    advertise atom tweed

--- src/main/scala/net/databinder/sling/Press.scala
+++ src/main/scala/net/databinder/sling/Press.scala
@@ -48,6 +48,11 @@ trait Document extends TitledContent {
 }
 
 case class ShowDocument(toc: TOC, md: String, tweedy: Option[(String, List[JsValue])]) extends Document {
+  override def head = super.head ++ tweedy.map {
+    case (tweed, _) => <link title="Atom 1.0 Feed" rel="alternate" type="application/atom+xml" href={
+        "http://search.twitter.com/search.atom?" + Http ? Map("q" -> tweed) }/>
+  }
+	
   def body =
     <div id="content">
       <div class="container">
