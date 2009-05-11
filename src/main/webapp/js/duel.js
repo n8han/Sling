5b7a5e53a702276f585e6d34fcac48a4231f78cc
Nathan Hamblen <nathan@technically.us>
2009-05-11 00:14:13 -0400

    eval templating

--- src/main/webapp/js/duel.js
+++ src/main/webapp/js/duel.js
@@ -1,4 +1,20 @@
 function makeHtml(md) {
-  return new Showdown.converter().makeHtml('' + md) 
+  return new Showdown.converter().makeHtml(dueval(md)) 
 }
 
+function dueval(str) {
+  var re = /#{[^}]*}/g
+  
+  var text = str.split(re)
+  var code = str.match(re) || []
+  var out = [], t = true, c = true
+  while (t || c) {
+    if (t = text.shift()) out.push(t)
+    if (c = code.shift()) out.push(eval(c.substr(1)))
+  }
+  return out.join("")
+}
+
+function my_eval(str) {
+  return eval(str)
+}
