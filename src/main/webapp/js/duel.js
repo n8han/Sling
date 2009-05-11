d734c480291dc827e2b770f1c828b7b35f600522
Nathan Hamblen <nathan@technically.us>
2009-05-11 02:09:02 -0400

    princess rhino

--- src/main/webapp/js/duel.js
+++ src/main/webapp/js/duel.js
@@ -1,20 +1,22 @@
 function makeHtml(md) {
-  return new Showdown.converter().makeHtml(dueval(md)) 
+  return new Showdown.converter().makeHtml(dueval(md));
 }
 
 function dueval(str) {
-  var re = /#{[^}]*}/g
+  var re = /#\{[^\}]*\}/g;
   
-  var text = str.split(re)
-  var code = str.match(re) || []
-  var out = [], t = true, c = true
-  while (t || c) {
-    if (t = text.shift()) out.push(t)
-    if (c = code.shift()) out.push(eval(c.substr(1)))
+  var text = ('' + str).split(re);
+  var code = str.match(re) || [];
+  var out = [], i = 0, j = 0;
+  while (i < text.length || j < code.length) {
+    if (i < text.length) out.push(text[i]);
+    if (j < code.length) try {
+      out.push(eval(code[j].substr(1)));
+    } catch (err) {
+      out.push("[" + err + "]")
+    }
+    i++; j++;
   }
-  return out.join("")
+  return out.join("");
 }
 
-function my_eval(str) {
-  return eval(str)
-}
