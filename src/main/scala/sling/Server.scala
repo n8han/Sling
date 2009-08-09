937378e2a0f541b240ac9d817c94622f6d9030e1
Nathan Hamblen <nathan@technically.us>
2009-08-09 19:02:54 -0400

    load servlet directly

--- src/main/scala/sling/Server.scala
+++ src/main/scala/sling/Server.scala
@@ -2,7 +2,8 @@ package sling
 
 import org.mortbay.jetty.Connector
 import org.mortbay.jetty.{Server => JettyServer}
-import org.mortbay.jetty.webapp.WebAppContext
+import org.mortbay.jetty.handler.ContextHandlerCollection
+import org.mortbay.jetty.servlet.{ServletHolder, Context}
 import org.mortbay.jetty.ajp.Ajp13SocketConnector
 
 import net.lag.configgy.Configgy
@@ -14,7 +15,14 @@ object Server {
     conn.setPort(Configgy.config.getInt("jetty.ajp.port", 9000))
     val server = new JettyServer()
     server.addConnector(conn)
-    server.addHandler(new WebAppContext(server, "target/webapp", "/"))
+    val contexts = new ContextHandlerCollection
+    server.setHandler(contexts)
+
+    val context = new Context(contexts,"/")
+    val holder = new ServletHolder(new slinky.http.servlet.StreamStreamServlet)
+    holder.setInitParameter("application", "sling.App")
+    context.addServlet(holder, "/*");
+
     server.setStopAtShutdown(true)
     server.start()
     server.join()
