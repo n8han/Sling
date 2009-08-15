8024731c139504efca32baf2d23dd69373becd3d
Nathan Hamblen <nathan@technically.us>
2009-08-15 14:50:22 -0400

    context cleanup

--- src/main/scala/sling/Server.scala
+++ src/main/scala/sling/Server.scala
@@ -3,7 +3,7 @@ package sling
 import org.mortbay.jetty.Connector
 import org.mortbay.jetty.{Server => JettyServer}
 import org.mortbay.resource.Resource
-import org.mortbay.jetty.handler.{ContextHandlerCollection, ResourceHandler}
+import org.mortbay.jetty.handler.ResourceHandler
 import org.mortbay.jetty.servlet.{ServletHolder, Context}
 import org.mortbay.jetty.ajp.Ajp13SocketConnector
 
@@ -11,27 +11,25 @@ import net.lag.configgy.Configgy
 
 object Server {
   def main(args: Array[String]) {
-    App // App initializes Configgy
+    App // this reference initializes Configgy
+    val server = new JettyServer()
+
     val conn = new Ajp13SocketConnector()
     conn.setPort(Configgy.config.getInt("jetty.ajp.port", 9000))
-    val server = new JettyServer()
     server.addConnector(conn)
 
-    val robots = getClass.getResource("/webroot/robots.txt").toString
-    val webroot = robots.substring(0, robots.lastIndexOf("/"))
-
-    val contexts = new ContextHandlerCollection
-    val context = new Context(contexts,"/")
-    val holder = new ServletHolder(new slinky.http.servlet.StreamStreamServlet)
-    holder.setInitParameter("application", "sling.App")
-    context.addServlet(holder, "/*")
-
+    val marker = getClass.getResource("/webroot/robots.txt").toString
+    val webroot = marker.substring(0, marker.lastIndexOf("/"))
     val resource_handler = new ResourceHandler
     val res = Resource.newResource(webroot)
     resource_handler.setBaseResource(res)
-
     server.addHandler(resource_handler)
-    server.addHandler(contexts)
+
+    val context = new Context(server,"/")
+    val holder = new ServletHolder(new slinky.http.servlet.StreamStreamServlet)
+    holder.setInitParameter("application", "sling.App")
+    context.addServlet(holder, "/*")
+    server.addHandler(context)
 
     server.setStopAtShutdown(true)
     server.start()
