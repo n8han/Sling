5a7ffaf7d36b545633918ea0d5fa03a299e5ddf5
Nathan Hamblen <nathan@technically.us>
2010-05-17 23:44:48 -0400

    ajp

--- src/main/scala/sling/Server.scala
+++ src/main/scala/sling/Server.scala
@@ -5,7 +5,7 @@ import net.lag.configgy.Configgy
 object Server {
   def main(args: Array[String]) {
     Configgy.configure("/etc/sling.conf")
-    unfiltered.server.Http(8080).resources(
+    unfiltered.server.Ajp(9000).resources(
         getClass.getResource("/webroot/robots.txt")
     ).filter(new App).start()
   }
